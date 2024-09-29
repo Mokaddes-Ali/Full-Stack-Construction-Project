6910818f8127ecc2015e409cbec6252136e553b4
mokadde <mokaddes.ru2000@gmail.com>
2024-09-29 10:28:27 +0600

    commit

--- React_Frontend/src/Pages/Home.jsx
+++ React_Frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../Components/Herosection'
+import Login from '../Components/Backend/login'
 
 const Home = () => {
   return (
     <div>
         <HeroSection />
+        <Login />
     </div>
   )
 }
