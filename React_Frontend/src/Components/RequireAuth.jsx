8cde94f954efab718b62e057deb6bd9d870fbbab
mokadde <mokaddes.ru2000@gmail.com>
2024-10-01 20:23:07 +0600

    commit

---
+++ React_Frontend/src/Components/RequireAuth.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AuthContext } from './Backend/context/Auth';
+import { Navigate } from 'react-router-dom';
+
+const RequireAuth = ({children}) => {
+
+    const {user} = useContext(AuthContext);
+
+    if(!user){
+        return  <Navigate to = 'admin/login' />
+    }
+
+  return children 
+  
+  
+}
+
+export default RequireAuth;
