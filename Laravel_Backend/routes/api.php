426f5c626e39444f524b08fc27bb74271f824f46
mokadde <mokaddes.ru2000@gmail.com>
2024-10-07 14:32:02 +0600

    commit

--- Laravel_Backend/routes/api.php
+++ Laravel_Backend/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\PostController;
+
 
 Route::post('register', [AuthenticationController::class, 'authenticate']);
 
@@ -23,4 +25,20 @@ Route::get('users', [UserController::class, 'index']);
 Route::get('users/{id}', [UserController::class, 'show']);
 Route::post('address', [UserController::class, 'store']);
 Route::post('updates', [UserController::class, 'update']);
+
+
+
+
+// Authentication Routes
+// Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+});
+
 
