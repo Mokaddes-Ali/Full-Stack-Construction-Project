148f7c32241d59267327d85b1f6c2e12c4228a11
mokadde <mokaddes.ru2000@gmail.com>
2024-10-07 19:28:36 +0600

    commit

--- Laravel_Backend/routes/api.php
+++ Laravel_Backend/routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\admin\ServicesController;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\admin\DashboardController;
-use App\Http\Controllers\PostController;
 
 
 Route::post('register', [AuthenticationController::class, 'authenticate']);
@@ -14,6 +15,7 @@ Route::post('register', [AuthenticationController::class, 'authenticate']);
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('dashboard', [DashboardController::class, 'index']);
     Route::get('logout', [AuthenticationController::class, 'logout']);
+    Route::post('services',[ServicesController::class, 'store']);
 
 });
 
@@ -21,10 +23,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('users', [UserController::class, 'index']);
-Route::get('users/{id}', [UserController::class, 'show']);
-Route::post('address', [UserController::class, 'store']);
-Route::post('updates', [UserController::class, 'update']);
+// Route::get('users', [UserController::class, 'index']);
+// Route::get('users/{id}', [UserController::class, 'show']);
+// Route::post('address', [UserController::class, 'store']);
+// Route::post('updates', [UserController::class, 'update']);
 
 
 
