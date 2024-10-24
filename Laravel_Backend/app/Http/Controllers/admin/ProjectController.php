b526f103262f2c245a6848543c41299db4ff5585
mokadde <mokaddes.ru2000@gmail.com>
2024-10-24 18:51:49 +0600

    commit

--- Laravel_Backend/app/Http/Controllers/admin/ProjectController.php
+++ Laravel_Backend/app/Http/Controllers/admin/ProjectController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers\admin;
 
 use App\Models\Project;
+use App\Models\TempImage;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+
 
 class ProjectController extends Controller
 {
@@ -20,7 +25,6 @@ class ProjectController extends Controller
 
     public function store(Request $request)
     {
-
         //dummy title
         // Str::singular($value)
 
@@ -53,7 +57,47 @@ class ProjectController extends Controller
         $project->status = $request->status;
         $project->save();
 
-        return response()->json([
+         //save temp image
+
+         if ($request->imageId > 0) {
+            $oldImage =   $project->image;
+
+           $tempImage = TempImage::find($request->imageId);
+           if ($tempImage != null) {
+               $extArray = explode('.',$tempImage->name);
+               $ext = last($extArray);
+
+               $fileName = strtotime('now'). $project->id.'.'.$ext;
+
+                // create new image instance (300 x 400)
+
+                     $sourcePath = public_path('uploads/temp/'.$tempImage->name);
+
+                      $destPath = public_path('uploads/projects/small/'.$fileName);
+                      $manager = new ImageManager(Driver::class);
+                      $image = $manager->read($sourcePath);
+                      $image -> coverDown(300, 400);
+                      $image -> save($destPath);
+
+                      //large image size
+
+                      $destPath = public_path('uploads/projects/large/'.$fileName);
+                      $manager = new ImageManager(Driver::class);
+                      $image = $manager->read($sourcePath);
+                      $image -> scaleDown(1200);
+                      $image -> save($destPath);
+
+                      $project->image = $fileName;
+                      $project->save();
+
+                      if ($oldImage != '') {
+                           File::delete(public_path('uploads/projects/small/'.$oldImage));
+                           File::delete(public_path('uploads/projects/large/'.$oldImage));
+                        }
+                      }
+             }
+
+         return response()->json([
             'status' => true,
             'message' => 'Project added successfully'
         ]);
