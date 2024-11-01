82cb3c74124a806254b22898e7ab2c7086d6c7a2
mokadde <mokaddes.ru2000@gmail.com>
2024-11-01 22:43:05 +0600

    commit

--- React_Frontend/src/Components/Backend/projects/edit.jsx
+++ React_Frontend/src/Components/Backend/projects/edit.jsx
@@ -110,137 +110,126 @@ const edit = ({ placeholder }) => {
   return (
     <>
      <AdminLayout>
-        <div className="flex min-h-screen bg-gray-700">
-          <div className="w-full bg-white p-8 rounded-md shadow-lg">
-            <h2 className="text-center text-2xl font-bold text-gray-900 mb-6">
-              Add New Project
-            </h2>
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-              {/* Service Name */}
-              <div>
-                <label className="block text-sm font-medium text-gray-900">Service Name</label>
-                <input
-                  type="text"
-                  {...register('title', { required: 'Service Name is required' })}
-                  placeholder="Service Name"
-                  className={`block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm ${errors.title ? 'ring-red-500 focus:ring-red-500' : ''}`}
-                />
-                {errors.title && <span className="text-sm text-red-600">{errors.title.message}</span>}
-              </div>
+    <div className="">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+         Services/Edit
+        </h2>
+      </div>
 
-              {/* Description */}
-              <div>
-                <label className="block text-sm font-medium text-gray-900">Description</label>
-                <textarea
-                  {...register('short_desc', { required: 'Description is required' })}
-                  placeholder="Description"
-                  className={`block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm ${errors.short_desc ? 'ring-red-500 focus:ring-red-500' : ''}`}
-                />
-                {errors.short_desc && <span className="text-sm text-red-600">{errors.short_desc.message}</span>}
-              </div>
-
-              {/* Content */}
-              <div>
-                <label className="block text-sm font-medium text-gray-900">Content</label>
-                <JoditEditor
-                  ref={editor}
-                  value={content}
-                  config={config}
-                  tabIndex={1} 
-                  onBlur={(newContent) => setContent(newContent)} 
-                />
-              </div>
-
-              {/* location */}
-              <div>
-                <label className="block text-sm font-medium text-gray-900">location</label>
-                <textarea
-                  {...register('location', { required: 'location is required' })}
-                  placeholder="location"
-                  className={`block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm ${errors.location ? 'ring-red-500 focus:ring-red-500' : ''}`}
-                />
-                {errors.location && <span className="text-sm text-red-600">{errors.location.message}</span>}
-              </div>
-
-              {/* sector */}
-              <div className="relative inline-block text-left">
-    <label className="block text-sm font-medium text-gray-900">Sector</label>
-    <select
-      {...register('sector', { required: 'Sector is required' })}
-      className={`block w-full mt-1 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm ${errors.sector ? 'ring-red-500 focus:ring-red-500' : ''}`}
-    >
-      <option value="">Select an option</option>
-      <option value="Option 1">Option 1</option>
-      <option value="Option 2">Option 2</option>
-      <option value="Option 3">Option 3</option>
-      <option value="Option 4">Option 4</option>
-      <option value="Option 5">Option 5</option>
-    </select>
-    {errors.sector && <span className="text-sm text-red-600">{errors.sector.message}</span>}
-  </div>
-
-              {/* construction_type */}
-              <div>
-  <label className="block text-sm font-medium text-gray-900">Construction Type</label>
-  <select
-    {...register('construction_type', { required: 'Construction type is required' })}
-    className={`block w-full mt-1 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm ${errors.construction_type ? 'ring-red-500 focus:ring-red-500' : ''}`}
-  >
-    <option value="">Select construction type</option>
-    <option value="Residential">Residential</option>
-    <option value="Commercial">Commercial</option>
-    <option value="Industrial">Industrial</option>
-    <option value="Institutional">Institutional</option>
-    <option value="Mixed-use">Mixed-use</option>
-  </select>
-  {errors.construction_type && <span className="text-sm text-red-600">{errors.construction_type.message}</span>}
-</div>
-
-
-              
-
-              {/* Slug */}
-              <div>
-                <label className="block text-sm font-medium text-gray-900">Slug</label>
-                <input
-                  type="text"
-                  {...register('slug', { required: 'Slug is required' })}
-                  placeholder="Slug"
-                  className={`block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm ${errors.slug ? 'ring-red-500 focus:ring-red-500' : ''}`}
-                />
-                {errors.slug && <span className="text-sm text-red-600">{errors.slug.message}</span>}
-              </div>
-
-              {/* Service Image */}
-              <div>
-                <label className="block text-sm font-medium text-gray-900">Project Image</label>
-                <input onChange={handleFile} type="file" className="mt-2" />
-              </div>
-
-              {/* Status */}
-              <div>
-                <label className="block text-sm font-medium text-gray-900">Status</label>
-                <select
-                  {...register('status', { required: 'Status is required' })}
-                  className="block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
-                >
-                  <option value="1">Active</option>
-                  <option value="0">Inactive</option>
-                </select>
-                {errors.status && <span className="text-sm text-red-600">{errors.status.message}</span>}
-              </div>
-
-              {/* Submit Button */}
-              <button
-                disabled={isDisable}
-                className="w-full flex justify-center bg-indigo-600 text-white font-semibold py-1.5 rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              >
-                Submit
-              </button>
-            </form>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          {/* Service Name */}
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Service Name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                {...register('title', { required: 'Service Name is required' })}
+                placeholder='Service Name'
+                className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.title ? 'ring-red-500 focus:ring-red-500' : ''}`}
+              />
+              {errors.title && (
+                <span className="text-sm text-red-600">{errors.title.message}</span>
+              )}
+            </div>
           </div>
-        </div>
-      </AdminLayout>
+
+          {/* Description */}
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Description
+            </label>
+            <div className="mt-2">
+              <textarea
+                {...register('short_desc', { required: 'Description is required' })}
+                placeholder='Description'
+                className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.short_desc ? 'ring-red-500 focus:ring-red-500' : ''}`}
+              />
+              {errors.short_desc && (
+                <span className="text-sm text-red-600">{errors.short_desc.message}</span>
+              )}
+            </div>
+          </div>
+
+          {/* Content */}
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Content
+            </label>
+            <div className="mt-2">
+              <JoditEditor
+                ref={editor}
+                value={content}
+                config={config}
+                tabIndex={1} 
+                onBlur={newContent => setContent(newContent)} 
+                onChange={newContent => {}} 
+              />
+            </div>
+          </div>
+
+          {/* Slug */}
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Slug
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                {...register('slug', { required: 'Slug is required' })}
+                placeholder='Slug'
+                className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.slug ? 'ring-red-500 focus:ring-red-500' : ''}`}
+              />
+              {errors.slug && (
+                <span className="text-sm text-red-600">{errors.slug.message}</span>
+              )}
+            </div>
+          </div>
+    {/* // Service Image */}
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">Service Image</label>
+            <br />
+            <input onChange={handleFile} type="file" />
+           
+            {
+            service.image && <img src={fileUrl+'uploads/services/small/'+service.image} alt = ""/>
+
+            }
+          </div>
+
+          {/* Status */}
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">Status</label>
+            <div className="mt-2">
+              <select
+                {...register('status', { required: 'Status is required' })}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="1">Active</option>
+                <option value="0">Inactive</option>
+              </select>
+              {errors.status && (
+                <span className="text-sm text-red-600">{errors.status.message}</span>
+              )}
+            </div>
+          </div>
+
+          {/* Submit Button */}
+          <div>
+            <button
+             disabled={isDisable}
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    </AdminLayout>
     </>
   )
 }
