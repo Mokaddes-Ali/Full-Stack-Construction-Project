39b9086243846c038ee0d61c4e1bb6aa03aed674
mokadde <mokaddes.ru2000@gmail.com>
2024-12-11 20:56:04 +0600

    commit

--- React_Frontend/src/Components/Frontend/StatsSection.jsx
+++ React_Frontend/src/Components/Frontend/StatsSection.jsx
@@ -5,7 +5,7 @@ import background from '../../assets/Images/background image.jpg'
 
 const StatsSection = () => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  const isInView = useInView(ref, { once: false });
 
   // Card data array
   const cardsData = [
@@ -54,11 +54,11 @@ const StatsSection = () => {
     <div className="w-full bg-cover bg-center bg-no-repeat" style={{
         backgroundImage: `url(${background})`
       }}>
-    <div className="lg:w-[95%] w-[90%] mx-auto px-4 py-12 flex flex-col justify-between items-center gap-8">
+    <div className="lg:w-[90%] w-[95%] mx-auto px-4 py-12 flex flex-col justify-between items-center gap-8">
       <h2 className="text-3xl md:text-4xl font-bold text-center text-gray-900 dark:text-white">Our Stats</h2>
     <div
       ref={ref}>
-      <div className=" w-full h-[350px] grid lg:grid-cols-4 md:grid-cols-2 grid-col-1 justify-center items-start gap-[20px] mt-[30px]">
+      <div className="w-full h-[350px] grid lg:grid-cols-4 md:grid-cols-2 grid-col-1 justify-center items-start gap-[20px] mt-[30px]">
         {cardsData.map((card, index) => (
           <motion.div
             key={card.id}
@@ -66,14 +66,18 @@ const StatsSection = () => {
             variants={cardVariants}
             initial="hidden"
             animate={isInView ? "visible" : "hidden"}
-            className="flex justify-center items-start gap-5 p-8 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 shadow-lg rounded-lg text-center transform transition-transform duration-300 hover:scale-105"
+            className="flex lg:flex-col justify-center items-start gap-5 p-8 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 shadow-lg rounded-lg text-center transform transition-transform duration-300 hover:scale-105"
           >
+            <div className="">
             <h2 className="text-5xl md:text-6xl font-bold text-gray-900 dark:text-white">
               {isInView && <CountUp start={0} end={card.count} duration={2} />}
               {card.suffix}
             </h2>
+            </div>
+            <div className="">
             <p className="text-lg md:text-xl font-semibold text-gray-700 dark:text-gray-300 mt-2">{card.label}</p>
             <p className="text-gray-600 dark:text-gray-400">{card.description}</p>
+            </div>
             <div className="absolute ml-36 w-16 h-16 bg-yellow-400 transform rotate-45 rounded"></div>
           </motion.div>
         ))}
