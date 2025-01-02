7232167cde8cb0301d8a9403609da0e930db248b
mokadde <mokaddes.ru2000@gmail.com>
2025-01-02 20:48:03 +0600

    commit

--- Laravel_Backend/database/seeders/UserSeeder.php
+++ Laravel_Backend/database/seeders/UserSeeder.php
@@ -9,29 +9,27 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-         $users=[
+        $users = [
             [
-                   'name' => 'Admin User',
-                   'email' => 'admin1@gmail.com',
-                   'password' => Hash::make('admin1234'),
-                   'role_id' =>1,
-                  ],
+                'name' => 'Admin User',
+                'email' => 'admin1@gmail.com',
+                'password' => Hash::make('admin1234'),
+                'role_id' => 1,
+            ],
 
-    [
-    'name' => 'Admin2 User',
-    'email' => 'admin2@gmail.com',
-     'password' => Hash::make('admin1234'),
-     'role_id' =>2,
-     ],
+            [
+                'name' => 'Admin2 User',
+                'email' => 'admin2@gmail.com',
+                'password' => Hash::make('admin1234'),
+                'role_id' => 2,
+            ],
 
         ];
 
-        foreach($users as $user){
+        foreach ($users as $user) {
             User::create($user);
         }
     }
