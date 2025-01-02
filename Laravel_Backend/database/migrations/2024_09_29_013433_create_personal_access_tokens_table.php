e107fdf968951e7733f03c8f304a586fcb8b8fb5
mokadde <mokaddes.ru2000@gmail.com>
2025-01-02 20:34:55 +0600

    commit

--- Laravel_Backend/database/migrations/2024_09_29_013433_create_personal_access_tokens_table.php
+++ Laravel_Backend/database/migrations/2024_09_29_013433_create_personal_access_tokens_table.php
@@ -1,4 +1,4 @@
-<?php
+vnvcc<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
--- Laravel_Backend/database/seeders/UserSeeder.php
+++ Laravel_Backend/database/seeders/UserSeeder.php
@@ -14,40 +14,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //   User::create([
-        //     'name' => 'mmh',
-        //     'email' => 'admin@Gmail.com',
-        //     'password' => Hash::make('123456789'),
-        //     'role_id' =>1,
-        // ]);
-
-        // User::create([
-        //     'name' => 'smith',
-        //     'email' => 'user@Gmail.com',
-        //     'password' => Hash::make('123456789'),
-        //     'role_id' =>2,
-        // ]);
-
-        $users=[
-
-
+         $users=[
             [
                    'name' => 'Admin User',
                    'email' => 'admin1@gmail.com',
                    'password' => Hash::make('admin1234'),
                    'role_id' =>1,
+                  ],
 
-            ],
-
-
-[
-
-
+    [
     'name' => 'Admin2 User',
     'email' => 'admin2@gmail.com',
      'password' => Hash::make('admin1234'),
      'role_id' =>2,
-]
+     ],
 
         ];
 
