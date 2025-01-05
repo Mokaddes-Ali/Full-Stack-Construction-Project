8aad230d378b23198514626c228beff35cdac737
mokadde <mokaddes.ru2000@gmail.com>
2025-01-05 07:10:47 +0600

    commit

--- React_Frontend/src/Components/New.jsx
+++ React_Frontend/src/Components/New.jsx
@@ -3,16 +3,15 @@ import React from 'react'
 const New = () => {
   return (
     <>
-    <div className="bg-blue-gray-50">
-   <div className="container">
+    <div className="bg-blue-500">
+   <div className="container mx-auto">
+    <div className="bg-green-300 h-96 flex">
     <div className="bg-red-300 h-80">
-
     </div>
     <div className="bg-deep-orange-600 h-96">
-
     </div>
-
    </div>
+    </div>
     </div>
     </>
   )
