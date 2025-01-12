57e1994adb2ed563f1e97904f8f6150604e986c1
mokadde <mokaddes.ru2000@gmail.com>
2025-01-12 14:39:48 +0600

    commit

--- Laravel_Backend/routes/api.php
+++ Laravel_Backend/routes/api.php
@@ -101,7 +101,7 @@ Route::get('articles', [ArticleController::class, 'index']);
 Route::post('articles/store', [ArticleController::class, 'store']);
 Route::get('articles/{id}', [ArticleController::class, 'show']);
 Route::put('articles/{id}', [ArticleController::class, 'update']);
-Route::delete('articles/{id}', [ArticleController::class, 'destroy']);
+Route::delete('articles/delete/{id}', [ArticleController::class, 'destroy']);
 
 
 Route::get('/user', function (Request $request) {
